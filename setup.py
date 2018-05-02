eedeb7d103e8accb65bcf640511eab61de92af96
lyuhao <lyhumji@gmail.com>
2018-05-01 22:04:24 -0400

    Update

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name = "vaelib",
 	  author ="Yuhao Li and Xi Wang",
 	  author_email = "yl408@duke.edu",
 	  packages=['vaelib'],
-	  license='MIT'
+          url='github.com/lyuhao/vae_stat663/',
+	  license='BSD 3-Clause'
 	)
